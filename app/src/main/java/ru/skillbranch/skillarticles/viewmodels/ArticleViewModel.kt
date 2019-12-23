50a93c83ae49f679c56095a2a0f75ee344d3e5a3
SergeBashkatov <s.bashkatov@profsoft.pro>
2019-12-23 18:54:07 +0400

    added boockmark listener

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -128,26 +128,26 @@ class ArticleViewModel(
 }
 
 data class ArticleState(
-    val isAuth: Boolean = false,
-    val isLoadingContent: Boolean = true,
-    val isLoadingReviews: Boolean = true,
-    val isLike: Boolean = false,
-    val isBigText: Boolean = false,
-    val isBookmark: Boolean = false,
-    val isShowMenu: Boolean = false,
-    val isDarkMode: Boolean = false,
-    val isSearch: Boolean = false,
-    val searchQuery: String? = null,
-    val searchResults: List<Pair<Int, Int>> = emptyList(),
-    val searchPosition: Int = 0,
-    val shareLink: String? = null,
-    val title: String? = null,
-    val category: String? = null,
-    val categoryIcon: Any? = null,
-    val date: String? = null,
-    val author: Any? = null,
-    val poster: String? = null,
-    val content: List<Any> = emptyList(),
-    val reviews: List<AnimationUtils> = emptyList()
+    val isAuth: Boolean = false, // пользователь авторизован
+    val isLoadingContent: Boolean = true, // контент загружается
+    val isLoadingReviews: Boolean = true, // отзывы загружаются
+    val isLike: Boolean = false, // отмечено как Like
+    val isBookmark: Boolean = false, // в закладках
+    val isShowMenu: Boolean = false, // отображается меню
+    val isBigText: Boolean = false, // шрифт увеличен
+    val isDarkMode: Boolean = false, // темный режим
+    val isSearch: Boolean = false, // режим поиска
+    val searchQuery: String? = null, // поисковый запрос
+    val searchResults: List<Pair<Int, Int>> = emptyList(), // результаты поиска (стартовая и конечная позиции)
+    val searchPosition: Int = 0, // текущая позиция найденного результата
+    val shareLink: String? = null, // ссылка Share
+    val title: String? = null, // заголовок статьи
+    val category: String? = null, // категория
+    val categoryIcon: Any? = null, // иконка категории
+    val date: String? = null, // дата публикации
+    val author: Any? = null, // автор статьи
+    val poster: String? = null, // обложка статьи
+    val content: List<Any> = emptyList(), // контент
+    val reviews: List<Any> = emptyList() // комментарии
 )
 
