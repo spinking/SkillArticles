427fae71af1e5aed0d890ff1c9915a55353e645d
SergeBashkatov <s.bashkatov@profsoft.pro>
2020-02-14 11:54:17 +0400

    added tests

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,9 +1,7 @@
 package ru.skillbranch.skillarticles.extensions
 
-fun String.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
-
-
-
+fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
+    if(this.isNullOrEmpty() || substr.isEmpty()) return listOf()
     val res = mutableListOf<Int>()
     val result = Regex(if(ignoreCase) substr.toLowerCase() else substr).findAll(if(ignoreCase) this.toLowerCase() else this, 0)
     for (i in result) {
