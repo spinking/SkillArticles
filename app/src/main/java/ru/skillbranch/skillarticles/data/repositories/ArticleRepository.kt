d8c5f700aa7c4f2ae32e5d7d6cd319d87df9b2db
Serge Bashkatov <splnklng@yandex.ru>
2020-05-11 16:37:38 +0400

    refactored article repository and gradle

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
@@ -3,7 +3,12 @@ package ru.skillbranch.skillarticles.data.repositories
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import androidx.paging.DataSource
+import androidx.paging.ItemKeyedDataSource
 import ru.skillbranch.skillarticles.data.*
+import ru.skillbranch.skillarticles.data.models.*
+import java.lang.Thread.sleep
+import kotlin.math.abs
 
 object ArticleRepository {
     private val local = LocalDataHolder
@@ -33,4 +38,74 @@ object ArticleRepository {
     }
 
     fun isAuth(): MutableLiveData<Boolean> = local.isAuth()
+
+    fun allComments(articleId: String, totalCount: Int) =
+        CommentsDataFactory(
+            itemProvider = ::loadContentsByRange,
+            articleId = articleId,
+            totalCount = totalCount
+        )
+
+    private fun loadContentsByRange(slug: String?, size: Int, articleId: String): List<CommentItemData> {
+        val data = network.commentsData.getOrElse(articleId) { mutableListOf() }
+        return when {
+            slug == null -> data.take(size)
+
+            size > 0 -> data.dropWhile { it.slug != slug }
+                .drop(1)
+                .take(size)
+            size < 0 -> data
+                .dropLastWhile { it.slug != slug }
+                .dropLast(1)
+                .takeLast(abs(size))
+            else -> emptyList()
+        }.apply { sleep(500) }
+    }
+
+    fun sendComment(articleId: String, comment: String, anserToSlug: String?) {
+        network.sendMessage(
+            articleId, comment, anserToSlug,
+            User("777", "John Doe", "https://skill-branch.ru/img/mail/bot/android-category.png")
+        )
+        local.incrementCommentsCount(articleId)
+    }
+}
+
+class CommentsDataFactory(
+    private val itemProvider: (String?, Int, String) -> List<CommentItemData>,
+    private val articleId: String,
+    private val totalCount: Int
+) : DataSource.Factory<String?, CommentItemData>() {
+    override fun create(): DataSource<String?, CommentItemData> =
+        CommentDadaSource(itemProvider, articleId, totalCount)
+}
+
+class CommentDadaSource(
+    private val itemProvider: (String?, Int, String) -> List<CommentItemData>,
+    private val articleId: String,
+    private val totalCount: Int
+) : ItemKeyedDataSource<String, CommentItemData>() {
+    override fun loadInitial(
+        params: LoadInitialParams<String>,
+        callback: LoadInitialCallback<CommentItemData>
+    ) {
+        val result = itemProvider(params.requestedInitialKey, params.requestedLoadSize, articleId)
+        callback.onResult(
+            if(totalCount > 0) result else emptyList(),
+            0,
+            totalCount
+        )
+    }
+
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<CommentItemData>) {
+        val result = itemProvider(params.key, params.requestedLoadSize, articleId)
+        callback.onResult(result)
+    }
+
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<CommentItemData>) {
+        val result = itemProvider(params.key, -params.requestedLoadSize, articleId)
+        callback.onResult(result)
+    }
+
+    override fun getKey(item: CommentItemData): String = item.slug
 }
