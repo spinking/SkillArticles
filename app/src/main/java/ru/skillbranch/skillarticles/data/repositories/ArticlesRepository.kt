6dde046eed3430624157fc2dcaabdb5cb8c2fcb3
Serge Bashkatov <splnklng@yandex.ru>
2020-05-11 17:04:39 +0400

    added articles repository

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticlesRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticlesRepository.kt
@@ -1,9 +1,90 @@
 package ru.skillbranch.skillarticles.data.repositories
 
 import androidx.lifecycle.LiveData
-import ru.skillbranch.skillarticles.data.ArticleItemData
+import androidx.paging.DataSource
+import androidx.paging.PositionalDataSource
 import ru.skillbranch.skillarticles.data.LocalDataHolder
+import ru.skillbranch.skillarticles.data.NetworkDataHolder
+import ru.skillbranch.skillarticles.data.models.ArticleItemData
+import java.lang.Thread.sleep
 
 object ArticlesRepository {
-    fun loadArticles(): LiveData<List<ArticleItemData>?> = LocalDataHolder.findArticles()
+
+    private val local = LocalDataHolder
+    private val network = NetworkDataHolder
+
+    fun allArticles() : ArticlesDataFactory =
+        ArticlesDataFactory(ArticleStrategy.AllArticles(::searchArticleByRange))
+
+    fun searchArticles(searchQuery: String) : ArticlesDataFactory =
+        ArticlesDataFactory(ArticleStrategy.SearchArticle(::searchArticleByTitle, searchQuery))
+
+    private fun searchArticleByTitle(start: Int, size: Int, queryTitle: String) = local.localArticleItems
+        .asSequence()
+        .filter { it.title.contains(queryTitle, true) }
+        .drop(start)
+        .take(size)
+        .toList()
+
+    private fun searchArticleByRange(start: Int, size: Int) = local.localArticleItems
+        .drop(start)
+        .take(size)
+
+    fun loadArticlesFromNetwork(start: Int, size: Int) : List<ArticleItemData> = network.networkArticleItems
+        .drop(start)
+        .take(size)
+        .apply { sleep(500) }
+
+    fun insertArticlesToDb(articles: List<ArticleItemData>) {
+        local.localArticleItems.addAll(articles)
+            .apply { sleep(100) }
+    }
+}
+
+class ArticlesDataFactory(
+    val strategy: ArticleStrategy
+) : DataSource.Factory<Int, ArticleItemData>() {
+    override fun create(): DataSource<Int, ArticleItemData> {
+        return ArticleDataSource(strategy)
+    }
+}
+
+class ArticleDataSource(
+    private val strategy: ArticleStrategy
+) : PositionalDataSource<ArticleItemData>() {
+    override fun loadInitial(
+        params: LoadInitialParams,
+        callback: LoadInitialCallback<ArticleItemData>
+    ) {
+        val result = strategy.getItems(params.requestedStartPosition, params.requestedLoadSize)
+        callback.onResult(result, params.requestedStartPosition)
+    }
+
+    override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<ArticleItemData>) {
+        val result = strategy.getItems(params.startPosition, params.loadSize)
+        callback.onResult(result)
+    }
+}
+
+sealed class ArticleStrategy() {
+    abstract fun getItems(start: Int, size: Int) : List<ArticleItemData>
+
+    class AllArticles(
+        private val itemProvider: (Int, Int) -> List<ArticleItemData>
+    ) : ArticleStrategy() {
+        override fun getItems(start: Int, size: Int): List<ArticleItemData> {
+            return itemProvider(start, size)
+        }
+    }
+
+    class SearchArticle(
+        private val itemProvider: (Int, Int, String) -> List<ArticleItemData>,
+        private val query: String
+    ) : ArticleStrategy() {
+        override fun getItems(start: Int, size: Int): List<ArticleItemData> {
+            return itemProvider(start, size, query)
+        }
+    }
+
+    //TODO bookmaks Strategy
 }
