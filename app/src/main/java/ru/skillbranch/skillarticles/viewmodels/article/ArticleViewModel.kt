b2cda3d0e5277707a0d3468afc194d175d08a745
Serge Bashkatov <spinking@mail.ru>
2020-11-28 19:49:22 +0400

    changed project tree

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/article/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/article/ArticleViewModel.kt
@@ -4,10 +4,7 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.*
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import ru.skillbranch.skillarticles.data.models.CommentItemData
 import ru.skillbranch.skillarticles.data.remote.res.CommentRes
 import ru.skillbranch.skillarticles.data.repositories.ArticleRepository
 import ru.skillbranch.skillarticles.data.repositories.CommentsDataFactory
